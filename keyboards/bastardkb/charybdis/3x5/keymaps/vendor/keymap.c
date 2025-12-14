bf7dbe926221131cd7496ce3332a710c0cc03229
Alexander Soto <alexandermsb@gmail.com>
2025-12-14 10:41:09 -0500

    tst error

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#error "ESTE KEYMAP ES EL QUE SE ESTA COMPILANDO"
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 #    include "timer.h"
