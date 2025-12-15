89b389bc15db0e31c5e6da1e1b5ab0ec71603457
Alexander Soto <alexandermsb@gmail.com>
2025-12-15 15:23:45 -0500

    add endif

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -283,7 +283,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 #    endif // CHARYBDIS_AUTO_SNIPING_ON_LAYER
-
+#endif     // POINTING_DEVICE_ENABLE  ← AÑADE ESTA LÍNEA
 // ========== AGREGAR AQUÍ LA FUNCIÓN PROCESS_RECORD_USER ==========
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
